8421090e3ce02a4dfe430de97d32ca1a545c3283
SYellops01 <sam.yellops@gmail.com>
2025-10-26 14:50:45 +0000

    Update streamlit_app.py
    Bug fix (indentation)

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if len(ingredients_list)>0:
     for fruits_chosen in ingredients_list:
         ingredients_string +=fruits_chosen + ', '
         #pull nutritional information for each fruit chosen
-      st.subheader(fruits_chosen+' Nutrition Information')
+        st.subheader(fruits_chosen+' Nutrition Information')
         smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/"+fruits_chosen)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     ingredients_string=ingredients_string[:len(ingredients_string)-2]
