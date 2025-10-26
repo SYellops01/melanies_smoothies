4d42b9dfefea82b3862d9bd2904bc723258d973a
SYellops01 <sam.yellops@gmail.com>
2025-10-26 15:00:27 +0000

    Update streamlit_app.py
    Improve search feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ name_on_order=st.text_input('Please enter your name for this order: ')
 #Set session and dataframe to pull fruit name from table in database
 cnx=st.connection("snowflake")
 session=cnx.session()
-my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe=session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 
 #create multi-select button to choose ingredients.Impose max 5 selections on user
 ingredients_list=st.multiselect("Choose up to 5 ingredients: ", my_dataframe, max_selections=5)
